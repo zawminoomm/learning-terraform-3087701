b5e330675b6d8a948b24bfac5888969dc78882fb
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 17:14:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
