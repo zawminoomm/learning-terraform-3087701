eaf5075e1181025fc2876899b320574e548ef7f7
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 18:43:36 +0800

    autoscaling

--- main.tf
+++ main.tf
@@ -30,15 +30,19 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.7.0"
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module_blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  
 }
 
 module "alb" {
