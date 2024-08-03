a0583f216ab14884701af0cccde477827f6ade40
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 16:32:24 +0800

    change vpc module

--- main.tf
+++ main.tf
@@ -14,13 +14,27 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -31,7 +45,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
   name = "blog"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
