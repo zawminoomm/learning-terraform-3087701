e651662642901c3901e4f1fed10b8d2d28c3bb5e
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 18:23:10 +0800

    change

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
-  subnets            = moudle.blog_vpc.public_subnets
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = module.blog_sg.security_group_id
 
   access_logs = {
