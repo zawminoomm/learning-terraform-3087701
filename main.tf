44b893004816c9dc6dd0ad25ab9ce7f7c1b454c0
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 18:21:10 +0800

    load balancer

--- main.tf
+++ main.tf
@@ -41,39 +41,48 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+odule "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
+  name = "blog-alb"
+
   load_balancer_type = "application"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
-  
-  target_groups = 
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = moudle.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
     {
       name_prefix      = "blog-"
-      backend_protocol         = "HTTP"
-      backend_port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
       targets = {
-        target_id        = aws_instance.blog.id
-        port = 80
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+        
       }
-      
     }
+  ]
 
-  
-  http_tcp_listeners = 
+  http_tcp_listeners = [
     {
-      port = 80
-      protocol = "HTTP"
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
-  
+  ]
+
   tags = {
-    Environment = "dev"
-    
+    Environment = "Dev"
   }
 }
 
