9ad7ba668edba195906cab1db6da6fa88a833e58
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 17:56:57 +0800

    load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
+  load_balancer_type = "application"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
