80c011b6daccc6d214010b593f3c56e82c1077a5
zawminoomm <37492582+zawminoomm@users.noreply.github.com>
2024-08-03 18:08:02 +0800

    ch

--- main.tf
+++ main.tf
@@ -64,13 +64,13 @@ module "alb" {
     }
 
   
-  http_tcp_listeners = [
+  http_tcp_listeners = 
     {
       port = 80
       protocol = "HTTP"
       target_group_index = 0
     }
-  ]
+  
   tags = {
     Environment = "dev"
     
